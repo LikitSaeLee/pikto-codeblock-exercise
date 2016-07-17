47704bf070168c72f536a92974009a364a26983a
LikitSaeLee <likit.usm@gmail.com>
2016-07-18 04:54:10 +0800

    bump answer

--- alphabet_tracker.rb
+++ alphabet_tracker.rb
@@ -1,2 +1,17 @@
 class AlphabetTracker
+  @@tracker_count = 0
+  attr_reader :alp_list
+
+  def initialize( alp_list )
+    @alp_list = alp_list
+    @@tracker_count += 1
+  end
+
+  def self.tracker_count
+    @@tracker_count
+  end
+
+  def transform_list
+    @alp_list = self.alp_list.map { |c| yield( c ) }
+  end
 end
--- number_tracker.rb
+++ number_tracker.rb
@@ -1,2 +1,17 @@
 class NumberTracker
+  @@tracker_count = 0
+  attr_reader :num_list
+
+  def initialize( num_list )
+    @num_list = num_list
+    @@tracker_count += 1
+  end
+
+  def self.tracker_count
+    @@tracker_count
+  end
+
+  def transform_list
+    @num_list = self.num_list.map { |n| yield( n ) }
+  end
 end
