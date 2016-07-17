875e3740ad03109df062b336ee449f5523b167d1
LikitSaeLee <likit.usm@gmail.com>
2016-07-18 04:53:06 +0800

    bump

--- spec/number_tracker_spec.rb
+++ spec/number_tracker_spec.rb
@@ -6,6 +6,13 @@ describe NumberTracker do
   let( :integers ) { [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ] }
   let( :number_tracker ) { NumberTracker.new( integers ) }
 
+  describe '.tracker_count' do
+    it 'count the number of initialized number tracker instance' do
+      3.times { NumberTracker.new( integers ) }
+      expect( NumberTracker.tracker_count ).to eq 3
+    end
+  end
+
   describe 'instance states' do
     it 'store an array of integer' do
       expect( number_tracker.num_list ).to eq( [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ] )
@@ -18,13 +25,6 @@ describe NumberTracker do
       expect( number_tracker.num_list ).to eq( [ 2, 4, 6, 8, 10, 12, 14, 16, 18 ] )
     end
   end
-
-  describe '.tracker_count' do
-    it 'count the number of initialized number tracker instance' do
-      3.times { NumberTracker.new( integers ) }
-      expect( NumberTracker.tracker_count ).to eq 3
-    end
-  end
 end
 
 # Exercise 2
@@ -33,6 +33,13 @@ end
 describe AlphabetTracker, skip: true do
   let( :alphabets ) { [ 'a', 'e', 'i', 'o', 'u' ] }
   let( :alphabet_tracker ) { AlphabetTracker.new( alphabets ) }
+
+  describe '.tracker_count' do
+    it 'count the number of initialized number tracker instance' do
+      3.times { AlphabetTracker.new( alphabets ) }
+      expect( AlphabetTracker.tracker_count ).to eq 3
+    end
+  end
 
   describe 'instance states' do
     it 'store an array of alphabet' do
@@ -43,14 +50,7 @@ describe AlphabetTracker, skip: true do
   describe '#transform_list' do
     it 'transform the alphabet list by taking custom expression' do
       alphabet_tracker.transform_list { |c| c*2 }
-      expect( alphabet_tracker.num_list ).to eq( [ 'aa', 'ee', 'ii', 'oo', 'uu' ] )
-    end
-  end
-
-  describe '.tracker_count' do
-    it 'count the number of initialized number tracker instance' do
-      3.times { AlphabetTracker.new( integers ) }
-      expect( AlphabetTracker.tracker_count ).to eq 3
+      expect( alphabet_tracker.alp_list ).to eq( [ 'aa', 'ee', 'ii', 'oo', 'uu' ] )
     end
   end
 end
